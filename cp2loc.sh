e1df6828dd0c88eac466d0aaba9e7d30b21eb698
Anton <anton.kuiper@gmail.com>
2020-11-22 14:19:20 +0100

    x

--- cp2loc.sh
+++ cp2loc.sh
@@ -1,2 +1,2 @@
-cp flaskapp.py /home/anton/flask_app/flaskapp.py
-cp webapp.wsgi /home/anton/flask_app/webapp.wsgi
+cp flaskapp.py /home/anton/flaskapp/flaskapp.py
+cp webapp.wsgi /home/anton/flaskapp/webapp.wsgi
