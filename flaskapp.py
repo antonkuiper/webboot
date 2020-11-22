87a513457455b85a307cdd4db06551175b68eca5
Anton <anton.kuiper@gmail.com>
2020-11-22 13:35:31 +0100

    changes from example
    changes from example

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,4 @@
+# https://asdkazmi.medium.com/deploying-flask-app-with-wsgi-and-apache-server-on-ubuntu-20-04-396607e0e40f
 # flaskapp.py
 # This is a "hello world" app sample for flask app. You may have a different file.
 from flask import Flask
@@ -7,4 +8,3 @@ def hello_world():
    return 'Hello from Flask!' 
 if __name__ == '__main__':
    app.run()
-   
--- webapp.wsgi
+++ webapp.wsgi
@@ -1,3 +1,4 @@
+# https://asdkazmi.medium.com/deploying-flask-app-with-wsgi-and-apache-server-on-ubuntu-20-04-396607e0e40f
 import sys 
 sys.path.insert(0, '/var/www/html/flaskapp')
 from flaskapp import app as application
